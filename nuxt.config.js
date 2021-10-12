dc0dd38d354a7814720ef8e58d6d3758f657e711
richardingwe <richardingwe5@gmail.com>
2021-10-12 19:52:36 +0100

    added style to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/default.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -43,4 +44,4 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
-}
+};
